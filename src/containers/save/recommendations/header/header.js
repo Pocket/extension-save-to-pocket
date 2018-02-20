952f684117a449693cacdfdbcffaf24a02e29dfd
Joel Kelly <joel@getpocket.com>
2018-02-20 14:52:48 -0800

    Adding Feed  check on recommendation header

--- src/containers/save/recommendations/header/header.js
+++ src/containers/save/recommendations/header/header.js
@@ -4,37 +4,38 @@ import Loading from './loading'
 import { localize } from '../../../../common/_locales/locales'
 
 const hasRecs = recs => recs
-const hasFeed = recs => recs && recs.feed.length > 0
+const hasFeed = recs => recs && recs.feed && recs.feed.length > 0
 
-export default function Header( recs ){
+export default function Header(recs) {
     return (
         <div>
-            {!hasRecs(recs) &&
-                <Loading />
-            }
+            {!hasRecs(recs) && <Loading />}
 
-            {hasRecs(recs) &&
+            {hasRecs(recs) && (
                 <div>
-                    {hasFeed(recs) &&
+                    {hasFeed(recs) && (
                         <div className={styles.header}>
-                            { recs.reason
-                                ? `${localize('recommendations','more_on')} ${recs.reason}`
-                                : localize('recommendations','explore')
-                            }
+                            {recs.reason
+                                ? `${localize('recommendations', 'more_on')} ${
+                                      recs.reason
+                                  }`
+                                : localize('recommendations', 'explore')}
                         </div>
-                    }
+                    )}
 
-                    {!hasFeed(recs) &&
+                    {!hasFeed(recs) && (
                         <div className={styles.header}>
                             {localize('recommendations', 'more_stories_detail')}
-                            <a href="https://getpocket.com/a/recommended/?src=ext_recs"
-                                rel="noopener noreferrer" target="_blank">
-                                {localize('recommendations','explore')}
+                            <a
+                                href="https://getpocket.com/a/recommended/?src=ext_recs"
+                                rel="noopener noreferrer"
+                                target="_blank">
+                                {localize('recommendations', 'explore')}
                             </a>
                         </div>
-                    }
+                    )}
                 </div>
-            }
+            )}
         </div>
     )
 }
