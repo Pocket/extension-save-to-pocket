fc18b8eaf3e7e5217cf65e63cdce175fe7d8e61a
Joel Kelly <joel.kelly@me.com>
2018-04-12 19:58:29 -0700

    Moving spoc to its own component

--- src/containers/save/recommendations/list/item.js
+++ src/containers/save/recommendations/list/item.js
@@ -5,7 +5,6 @@ import classNames from 'classnames/bind'
 import * as Icon from '../../../../components/icons'
 import { domainForUrl } from '../../../../common/utilities'
 import { localize } from '../../../../common/_locales/locales'
-import SpocHeader from './item.spoc.header'
 
 const cx = classNames.bind(styles)
 const copy = {
@@ -26,23 +25,8 @@ export default class RecommendationItem extends Component {
     return copy[this.props.item.status]
   }
 
-  get spocContext() {
-    const item = this.props.item
-    return {
-      cxt_impression_id: item.impression_id,
-      cxt_view: 'extension_ad',
-      cxt_feed_item: item.feed_item_id,
-      cxt_index: item.sort_id,
-      cxt_post_id: item.post_id
-    }
-  }
-
   onClick = () => {
     const item = this.props.item
-
-    if (item.isSpoc) {
-      this.props.spocClick({ context: this.spocContext })
-    }
 
     this.props.openRecommendation({
       tabId: this.props.tabId,
@@ -76,21 +60,6 @@ export default class RecommendationItem extends Component {
     return (
       <li style={this.props.motionStyle} className={recommendationItemClass}>
         <div className={itemContainerClass}>
-          {item.isSpoc && (
-            <SpocHeader
-              tabId={this.props.tabId}
-              itemId={item.id.toString()}
-              sponsorurl={item.url}
-              sponsor={item.sponsor}
-              avatar={item.avatar}
-              spocContext={this.spocContext}
-              spocImpression={this.props.spocImpression}
-              spocView={this.props.spocView}
-              spocClick={this.props.spocClick}
-              spocRemove={this.props.spocRemove}
-            />
-          )}
-
           {item.has_image && (
             <div className={styles.image} style={this.imageStyle} />
           )}
