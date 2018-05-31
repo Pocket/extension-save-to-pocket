54a82250fe187c67a5c66e0bae72cd8becbb5fd8
Joel Kelly <joel.kelly@me.com>
2018-05-31 15:19:47 -0700

    Adding Color components
    In future we are moving away from SCSS and will pull colors in directly

---
+++ src/Components/Elements/Colors/colors.js
@@ -0,0 +1,25 @@
+// coffee: '#C0FFEE'
+export const Colors = {
+  hotCoral: '#ef4056',
+  amber: '#FCB643',
+  mintGreen: '#83EDB8',
+  teal: '#50BCB6',
+  emerald: '#00a69d'
+}
+
+export const Shades = {
+  black: '#000000',
+  night: '#1f1f1f',
+  pitch: '#222222',
+  tar: '#333333',
+  coal: '#444444',
+  darksmoke: '#666666',
+  overcast: '#999999',
+  silver: '#bbbbbb',
+  gray: '#cccccc',
+  smoke: '#dddddd',
+  alabaster: '#e6e6e6',
+  snow: '#eeeeee',
+  powder: '#f9f9f9',
+  white: '#ffffff'
+}
---
+++ src/Components/Elements/Colors/colors.stories.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled, { css } from 'react-emotion'
+import { storiesOf } from '@storybook/react'
+import { Colors, Shades } from './colors'
+import { DoorHangerBase } from '../Foundations/foundation'
+
+const ColorDisplay = styled('div')`
+  ${DoorHangerBase};
+  display: inline-flex;
+  flex-direction: column;
+  width: 12em;
+  margin-right: 15px;
+  margin-bottom: 15px;
+  padding: 6px;
+  justify-content: center;
+  overflow: hidden;
+`
+
+const ColorBlock = styled('div')`
+  background-color: ${props => (props.color ? props.color : '#f7f7f7')};
+  display: block;
+  height: 5em;
+  width: 100%;
+  border-radius: 4px 4px 0 0;
+`
+
+const ColorContent = css`
+  font-size: 12px;
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+  padding: 10px 0;
+  text-transform: uppercase;
+`
+
+const ColorName = styled('div')`
+  ${ColorContent};
+  border-bottom: 1px solid ${Shades.snow};
+`
+const ColorDetail = styled('div')`
+  ${ColorContent};
+  display: block;
+`
+
+storiesOf('Elements|Colors', module)
+  .addDecorator(story => <div>{story()}</div>)
+  .add('Colors', () => displayColors({ Colors, Shades }))
+
+function displayColors(colorObject) {
+  return Object.keys(colorObject).map(colorType => {
+    const colors = colorObject[colorType]
+
+    return (
+      <div>
+        <h1
+          className={css`
+            font-weight: 100;
+          `}>
+          {colorType}
+        </h1>
+        {Object.keys(colors).map((color, index) => (
+          <ColorDisplay color={colors[color]} key={color + index}>
+            <ColorBlock color={colors[color]} />
+            <ColorName color={colors[color]}>{color}</ColorName>
+            <ColorDetail>Hex: {colors[color]}</ColorDetail>
+          </ColorDisplay>
+        ))}
+      </div>
+    )
+  })
+}
+
+/*
+          <ColorDisplay color={colors[color]} key={color + index}>
+            <ColorBlock color={colors[color]} />
+            <ColorName color={colors[color]}>{color}</ColorName>
+            <ColorDetail>Hex: {colors[color]}</ColorDetail>
+          </ColorDisplay>
+          */
