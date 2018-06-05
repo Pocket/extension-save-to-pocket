5a440162ce52c2c63b70bad6c484f8966d1a9931
Joel Kelly <joel.kelly@me.com>
2018-06-05 10:09:22 -0700

    Updating Storybook Loader to handle themes

--- .storybook/storyLoader.js
+++ .storybook/storyLoader.js
@@ -2,26 +2,32 @@ import React from 'react'
 import styled, { css } from 'react-emotion'
 import { storiesOf, addDecorator } from '@storybook/react'
 import { configure } from '@storybook/react'
+import { ThemeProvider } from 'emotion-theming'
+import { Themes } from 'Elements/Themes/themes'
 
 import { SVGSymbols } from 'Elements/Icons/icon.symbols'
 import { SVGForStories } from './svg.guides'
 
 addDecorator(story => {
   const content = story()
+  const themeArray = ['light', 'dark']
 
   const WrapStories = styled('div')`
-    padding: 2em;
+    margin: 2em;
     box-sizing: border-box;
     font-family: 'Proxima Nova', 'proxima-nova', -apple-system,
       BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
       'Helvetica Neue', Helvetica, sans-serif;
+    width: 320px;
   `
 
   return (
     <React.Fragment>
       <SVGForStories />
       <SVGSymbols />
-      <WrapStories>{content}</WrapStories>
+      <ThemeProvider theme={Themes['light']}>
+        <WrapStories>{content}</WrapStories>
+      </ThemeProvider>
     </React.Fragment>
   )
 })
