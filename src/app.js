a558159ecda59a6c38f1b7c954697d1eae77aa66
Joel Kelly <joel.kelly@me.com>
2018-07-02 08:54:24 -0700

    Wrapping store for use with chrome-react-redux

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Store } from 'react-chrome-redux'
 import { Provider } from 'react-redux'
 
-import { store } from './store'
+import { PORT_NAME } from 'Common/constants'
+import { sendMessage } from 'Common/interface'
+
 import { FrameObserver } from 'Containers/Frame/frame.observer'
 import { SavePanel } from 'Containers/Save/save.container.js'
 import { SVGSymbols } from 'Elements/Icons/icon.symbols'
 
+/* APP FRAME
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
 export class AppFrame extends Component {
   constructor(props) {
     super(props)
@@ -26,13 +31,39 @@ export class AppFrame extends Component {
   render() {
     return (
       <div ref={this.heightRef} style={{ width: '320px', padding: '10px' }}>
-        <SVGSymbols />
-        <Provider store={store}>
-          <SavePanel />
-        </Provider>
+        <SavePanel />
       </div>
     )
   }
 }
 
-ReactDOM.render(<AppFrame />, document.getElementById('pocket-extension-root'))
+/* MOUNT APP
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+function getExtensionInfo() {
+  return new Promise(resolve =>
+    sendMessage(null, { action: 'getExtensionInfo' }, resolve)
+  )
+}
+
+getExtensionInfo().then(info => {
+  const proxyStore = new Store({
+    portName: PORT_NAME,
+    extensionId: info.id
+  })
+
+  proxyStore.ready().then(() => {
+    document.addEventListener('click', function() {
+      sendMessage(null, { action: 'frameFocus', status: true })
+    })
+
+    ReactDOM.render(
+      <React.Fragment>
+        <SVGSymbols />
+        <Provider store={proxyStore}>
+          <AppFrame />
+        </Provider>
+      </React.Fragment>,
+      document.getElementById('pocket-extension-root')
+    )
+  })
+})
--- src/store.js
+++ src/store.js
@@ -4,6 +4,8 @@ import { all } from 'redux-saga/effects'
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
+import { wrapStore } from 'react-chrome-redux'
+import { PORT_NAME } from 'Common/constants'
 /* IMPORT CONTAINER STATES
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 import { activeTabSagas } from 'Containers/Background/tab.active.state'
@@ -28,4 +30,6 @@ const composeEnhancers = composeWithDevTools({ port: 8000 })
 
 const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware))
 export const store = createStore(rootReducer, {}, enhancers)
+
+wrapStore(store, { portName: PORT_NAME })
 sagaMiddleware.run(rootSaga)
