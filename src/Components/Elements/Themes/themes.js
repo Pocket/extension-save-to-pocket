020d091a252183512e8f3cf7f6990fd05b72a38f
Joel Kelly <joel.kelly@me.com>
2018-07-06 11:34:43 -0700

    Updating themes
    This needs work and structural consistency

--- src/Components/Elements/Themes/themes.js
+++ src/Components/Elements/Themes/themes.js
@@ -1,4 +1,4 @@
-import { Colors } from '../Colors/colors'
+import { Colors, Shades } from '../Colors/colors'
 
 export const Themes = {
   light: {
@@ -26,14 +26,25 @@ export const Themes = {
       }
     },
     chip: {
-      color: '#313131',
-      background: '#e6e6e6',
-      border: '#cccccc',
-      hover: { color: '#444', background: '#d5d5d5', border: '#aaa' },
+      color: Shades.pitch,
+      delete: Shades.overcast,
+      background: Shades.powder,
+      border: Shades.silver,
+      hover: {
+        color: '#444',
+        background: Shades.snow,
+        border: Shades.silver,
+        delete: '#000'
+      },
       active: {
+        delete: '#ffffff',
         color: '#ffffff',
-        background: Colors['teal'],
-        border: Colors['emerald']
+        background: Colors.teal,
+        border: Colors.emerald,
+        hover: {
+          background: Colors.emerald,
+          delete: '#000'
+        }
       }
     },
     article: { header: '#b1b2b2' },
@@ -45,89 +56,5 @@ export const Themes = {
       hover: { background: '#ccc', color: '#000' }
     },
     modeButton: { color: '#313131', background: '#ffffff', border: '#cccccc' }
-  },
-  dark: {
-    body: { background: '#1f1f1f', color: '#ccc', link: Colors['teal'] },
-    nav: {
-      color: '#313131',
-      background: '#fdfdfd',
-      border: '#e7e7e7',
-      shadow: '0 0 5px rgba(0, 0, 0, 0.25)',
-      hover: {
-        color: Colors['teal']
-      }
-    },
-    card: {
-      color: '#ccc',
-      background: '#292929',
-      border: '#333333',
-      shadow: '0 0 3px 1px rgba(0, 0, 0, 0.6)'
-    },
-    menu: {
-      color: '#313131',
-      background: '#292929',
-      hover: {
-        color: '#ffffff',
-        background: Colors['teal']
-      }
-    },
-    chip: {
-      color: '#cccccc',
-      background: '#909090',
-      border: '#333333',
-      hover: { color: '#cccccc', background: '#909090', border: '#333333' }
-    },
-    article: { header: '#909090' },
-    settings: {
-      background: '#ffffff',
-      color: '#444',
-      border: '#ccc',
-      hover: { background: '#ccc', color: '#000' }
-    },
-    modeButton: {
-      color: '#ccc',
-      background: '#1f1f1f',
-      border: '#333333'
-    }
-  },
-  sepia: {
-    body: { background: '#FBF0D9', color: '#5B4636', link: Colors['teal'] },
-    nav: {
-      color: '#313131',
-      background: '#fdfdfd',
-      border: '#e7e7e7',
-      shadow: '0 0 5px rgba(0, 0, 0, 0.25)',
-      hover: {
-        color: Colors['teal']
-      }
-    },
-    card: {
-      color: '#5B4636',
-      background: '#FEFDFB',
-      border: '#E6DECF',
-      shadow: '0 0 5px rgba(0, 0, 0, 0.25)'
-    },
-    menu: {
-      color: '#5B4636',
-      background: '#FEFDFB',
-      hover: {
-        color: '#ffffff',
-        background: Colors['teal']
-      }
-    },
-    chip: {
-      color: '#FBF0D9',
-      background: '#ad998b',
-      border: '#E6DECF',
-      hover: { color: '#FBF0D9', background: '#ad998b', border: '#E6DECF' }
-    },
-    article: { header: '#ad998b' },
-    settings: {
-      background: '#ffffff',
-      color: '#444',
-      border: '#ccc',
-      hover: { background: '#ccc', color: '#000' }
-    },
-    modeButton: { color: '#5B4636', background: '#FBF0D9', border: '#E6DECF' }
   }
 }
