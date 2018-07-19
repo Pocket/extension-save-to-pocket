86c90a8d67344f581461a04866722e9d2a2e8ff6
Nelson Omuto <nelsonomuto@gmail.com>
2018-07-19 11:51:51 -0700

    add post install executable to rename files, consider perhaps a git hook as well or in substitution of this

---
+++ .tooling/scripts/postinstall.sh
@@ -0,0 +1,13 @@
+# Rename folders to allow for case-sensitive file systems based on refactor to new update/structure-improvements
+
+mv src/common src/Common
+mv src/components src/Components
+mv src/containers src/Containers
+mv src/containers/auth src/containers/Auth
+mv src/containers/background src/containers/Background
+mv src/containers/options src/containers/Options
+mv src/containers/save src/containers/Save
+mv src/containers/save/recommendations src/containers/Save/Recommendations
+mv src/containers/save/survey src/containers/Save/Survey
+mv src/containers/save/toolbar src/containers/Save/Toolbar
+mv src/containers/sites src/containers/Sites
