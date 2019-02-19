746322f15cbcda9d25275da6948f826c12a2bdab
Nelson Omuto <nelsonomuto@gmail.com>
2019-02-19 12:28:48 -0500

    update snapshots

--- src/Components/Views/Toolbar/Overflow/overflowItem.js
+++ src/Components/Views/Toolbar/Overflow/overflowItem.js
@@ -33,10 +33,9 @@ const ItemButton = styled('button')`
 export function OverflowItem({ name, method, divider }) {
   return (
     <ItemWrapper divider={divider}>
-        <ItemButton onClick={method}>
-          <Icon name={name} margin="0 8px 0 0" />
-          {localize('actions', name)}
-        </ItemButton>
-      </ItemWrapper>
+      <ItemButton onClick={method}>
+        <Icon name={name} margin="0 8px 0 0" /> {localize('actions', name)}
+      </ItemButton>
+    </ItemWrapper>
   )
 }
