47705e540464d3f79f27319b73b40a55c5563a92
collectedmind <joel@getpocket.com>
2019-08-20 09:24:37 -0700

    refactor(auth): add request utility

--- _safari/Save to Pocket Extension/Common/Utilities.swift
+++ _safari/Save to Pocket Extension/Common/Utilities.swift
@@ -31,5 +31,57 @@ class Utilities: SafariExtensionHandler {
       })
     }
   }
-  
+
+  static func request(from page: SFSafariPage, userInfo: [String : Any]?) throws -> Data? {
+    guard let uri: String = userInfo?["url"] as? String, let url = URL(string: uri) else {
+      throw RequestErrors.url
+    }
+    
+    // Semaphor gives us the ability to emulate async/await
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    // Construct the URLRequest
+    var urlRequest = URLRequest(url: url)
+    urlRequest.httpMethod = "POST"
+    
+    // Add request parameters
+    if let parameters = userInfo?["parameters"] as? [ String : Any],
+      JSONSerialization.isValidJSONObject(parameters),
+      let jsonData = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) {
+      urlRequest.httpBody = jsonData
+      urlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+      urlRequest.setValue("application/json", forHTTPHeaderField: "X-Accept")
+    }
+    
+    // Variables to hold response
+    var data: Data?
+    var response: URLResponse?
+    var error: Error?
+    
+    // Make the request and capure the response
+    _ = URLSession.shared.dataTask(with: urlRequest){ rData, rResponse, rError in
+      data = rData
+      response = rResponse
+      error = rError
+      semaphore.signal()
+      }.resume()
+    
+    // Pause execution until we get the signal from the semaphore
+    _ = semaphore.wait(timeout: .distantFuture)
+    
+    // Once we get the response, check that it's valid?
+    if let restResponse = response as? HTTPURLResponse, restResponse.statusCode > 300 {
+      throw RequestErrors.statusCode
+    }
+    
+    // Are there any errors?
+    if error != nil {
+      throw RequestErrors.error
+    }
+    
+    // Excellent—Pass on the data
+    let string = String(data: data!, encoding: String.Encoding.utf8)
+    NSLog("Auth Success (string): (\(String(describing: string!)))")
+    return data
+  }
 }
