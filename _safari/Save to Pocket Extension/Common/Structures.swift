8d220d8ef48bb41177db4fcfb43df8b38b2e7b2d
collectedmind <joel@getpocket.com>
2019-08-20 09:25:54 -0700

    refactor(auth): add api calls to the server

---
+++ _safari/Save to Pocket Extension/Common/Structures.swift
@@ -0,0 +1,36 @@
+//
+//  SaveToPocket.structures.swift
+//  Save to Pocket Extension
+//
+//  Created by Joel Kelly on 8/19/19.
+//  Copyright © 2019 Pocket. All rights reserved.
+//
+
+struct GuidResponse: Decodable {
+  let guid: String
+}
+//
+struct AuthResponse: Decodable{
+  let access_token: String
+  let username: String?
+  let account: Account?
+}
+
+struct Account: Decodable{
+  let user_id: String
+  let username: String?
+  let email: String?
+  let birth: String?
+  let first_name: String?
+  let last_name: String?
+  let premium_status: String?
+  let profile: Profile?
+}
+
+struct Profile: Decodable{
+  let username: String?
+  let name: String?
+  let description: String?
+  let avatar_url: String?
+}
+
