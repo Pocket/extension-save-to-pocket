2d43101c6cc21f9ae5436a0b65dbf9fd3127dd9e
collectedmind <joel@getpocket.com>
2019-08-21 12:52:54 -0700

    chore(cleanup): adjusting code format
    Ideally we will be able to automate this in the future since it can be highly contentious.  The goal here is to make sure code is readable at the same widths as the rest of the code.  In the JS we auto-wrap at a specific width, which has the added benefit of encouraging concise and discrete code.

--- _safari/Save to Pocket Extension/Common/API.swift
+++ _safari/Save to Pocket Extension/Common/API.swift
@@ -36,36 +36,43 @@
 import SafariServices
 
 class SaveToPocketAPI: SafariExtensionHandler{
-  
-  static func getGuid(from page: SFSafariPage, completion: @escaping (Result<String, RequestError>) -> Void) -> Void {
-    // Get authorized from server
+
+  static func getGuid(
+    from page: SFSafariPage,
+    completion: @escaping (Result<String, RequestError>) -> Void
+  ) -> Void {
+
     // Build request data dictionary
     let requestData: [String : Any] = [
       "consumer_key": CONSUMER_KEY,
       "abt": "1"
     ]
-    
-    let requestInfo: [String : Any] = ["url" : "https://getpocket.com/v3/guid/",
-                                       "method" : "GET",
-                                       "parameters" : requestData
+
+    let requestInfo: [String : Any] = [
+      "url" : "https://getpocket.com/v3/guid/",
+      "method" : "GET",
+      "parameters" : requestData
     ];
-    
+
     NSLog("Request Guid: (\(String(describing: requestInfo)))")
+
     Utilities.request(from: page, userInfo: requestInfo) { result in
       switch result {
+
       case .success(let data):
         guard let guidJSON = try? JSONDecoder().decode(GuidResponse.self, from: data) else {
           completion(.failure(.json))
           return
         }
         completion(.success(guidJSON.guid))
+
       case .failure(let error):
         NSLog("Request Failed: (\(String(describing: requestInfo)))")
         completion(.failure(error))
       }
     }
   }
-  
+
   static func validateAuthCode(from page: SFSafariPage, userInfo: [String : Any]?, completion: @escaping (Result<String, RequestError>) -> Void) -> Void {
     
     // Since we got the Auth Code from the passed in page, we close that page
@@ -79,19 +86,21 @@ class SaveToPocketAPI: SafariExtensionHandler{
     
     // Create a serial queue.
     let queue = DispatchQueue(label: "API.validateAuthCode")
-    
+
     // Run all requests within queue.
     queue.async {
-      
+
       var guid: String? = nil
       var error: RequestError = .undefined
-      
+
       let waitOnGuid = DispatchSemaphore(value: 0)
-      
+
       getGuid(from: page) { result in
         switch result {
+
         case .failure(let e):
           error = e
+
         case .success(let g):
           guid = g
           NSLog("GUID: \(String(describing: guid))")
@@ -99,15 +108,15 @@ class SaveToPocketAPI: SafariExtensionHandler{
           waitOnGuid.signal()
         }
       }
-      
+
       // We can block this queue without affecting the main thread.
       _ = waitOnGuid.wait(timeout: .distantFuture)
-      
+
       guard guid != nil else {
         completion(.failure(error))
         return
       }
-      
+
       // Get authorized from server
       // Build request data dictionary
       let requestData: [String : Any] = [
@@ -118,27 +127,33 @@ class SaveToPocketAPI: SafariExtensionHandler{
         "account": "1",
         "grant_type": "extension"
       ]
-      
-      
-      let requestInfo: [String : Any] = ["url" : "https://getpocket.com/v3/oauth/authorize.php",
-                                         "method" : "POST",
-                                         "parameters" : requestData
+
+
+      let requestInfo: [String : Any] = [
+        "url" : "https://getpocket.com/v3/oauth/authorize.php",
+        "method" : "POST",
+        "parameters" : requestData
       ];
-      
-      NSLog("Auth Request: (\(String(describing: requestInfo)))")
+
+      //
+      NSLog("Request Auth: (\(String(describing: requestInfo)))")
       Utilities.request(from: page, userInfo: requestInfo) { result in
         switch result {
+
         case .failure(_):
           NSLog("Auth Failed: (\(String(describing: requestInfo)))")
+
         case .success(let data):
           guard let authJSON = try? JSONDecoder().decode(AuthResponse.self, from: data) else {
             NSLog("Auth Failed: (\(String(describing: requestInfo)))")
             completion(.failure(.auth))
             return
           }
+
           // Store Account data
           let defaults = UserDefaults.standard
           defaults.set(authJSON.access_token, forKey: "access_token")
+
           completion(.success(authJSON.access_token))
         }
       }
