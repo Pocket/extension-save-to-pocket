1e4defc765f66b1b6cb0d4efa5e4c569cf915777
Nelson Omuto <nelsonomuto@gmail.com>
2019-08-22 11:04:02 -0400

    refactor(styles): use emotion for all src/components

--- src/components/toggle/toggle.stories.js
+++ src/components/toggle/toggle.stories.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Toggle from './toggle'
 storiesOf('Toggle', module)
-  .add('active', () => <Toggle active={true}>click me!</Toggle>)
-  .add('inactive', () => <Toggle active={false}>click me!</Toggle>)
-  .add('darkmode', () => <Toggle darkMode={true}>click me!</Toggle>)
+  .add('active', () => <Toggle active={true}/>)
+  .add('inactive', () => <Toggle active={false}/>)
+  .add('darkmode', () => <Toggle darkMode={true}/>)
 
