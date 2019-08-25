899875b89e43187c4d4773c63d23d75b81da2e89
collectedmind <joel@getpocket.com>
2019-08-25 11:50:42 -0700

    refactor(config): removing custom config

--- config/jest/setup/main.js
+++
@@ -1,5 +0,0 @@
-require('./requestAnimationFrame.js')
-require('./legacyCodePolyfills.js')
-require('./setupEnzyme.js')
-const fs = require('fs-extra')
-fs.mkdirp('./.debug') // create debug directory used in tests
