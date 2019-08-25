c5cd9b1ff2ae97380983a7719aed29efd273e056
collectedmind <joel@getpocket.com>
2019-08-25 11:50:42 -0700

    refactor(config): removing custom config

--- config/jest/setup/requestAnimationFrame.js
+++
@@ -1,8 +0,0 @@
-// Polyfill for rAF.
-//
-// This removes the React 16 warning "React depends on requestAnimationFrame"
-// when using Enzyme with React 16.
-// TODO: Remove this when Enzyme adds support for React 16 (v3)
-window.requestAnimationFrame = function(cb) {
-  return setTimeout(cb, 0)
-}
