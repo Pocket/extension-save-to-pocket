17d7a96e2b6dd49072865ebecb858ed854a6c02a
collectedmind <joel@getpocket.com>
2019-08-26 12:27:28 -0700

    build(safari): building extension entry points

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,14 @@
-import { composeWithDevTools } from 'remote-redux-devtools'
+import {} from 'redux'
 import { wrapStore } from 'react-chrome-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import { PORT_NAME } from '../common/constants'
 import rootSaga from './combineSagas'
 import rootReducer from './combineReducers'
 
-const composeEnhancers = composeWithDevTools({ port: 8000 })
-
 export const initializeStore = () => {
   const sagaMiddleware = createSagaMiddleware()
-  const enhancers = composeEnhancers(applyMiddleware(sagaMiddleware))
+  const enhancers = compose(applyMiddleware(sagaMiddleware))
   const store = createStore(rootReducer, {}, enhancers)
 
   wrapStore(store, { portName: PORT_NAME })
