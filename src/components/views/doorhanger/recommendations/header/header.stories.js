c0321fc6aac8156b7f8b4a02c85ddb59cd6d2684
collectedmind <joel@getpocket.com>
2019-08-28 09:19:22 -0700

    refactor(recs): use recs mock and add story

--- src/components/views/doorhanger/recommendations/header/header.stories.js
+++ src/components/views/doorhanger/recommendations/header/header.stories.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Header from './header'
 storiesOf('Views | Recommendations/Header', module)
-  .add('w/ recs', () => <Header recs={{ reason: 'Chemistry', feed: [1, 2] }} />)
-  .add('w/o recs', () => <Header recs={{ reason: '' }} />)
+  .add('reason', () => <Header recs={{ reason: 'Chemistry', feed: [1, 2] }} />)
+  .add('no reason', () => <Header recs={{ reason: '', feed: [1, 2] }} />)
+  .add('no results', () => <Header recs={{ reason: '' }} />)
   .add('loading', () => <Header />)
