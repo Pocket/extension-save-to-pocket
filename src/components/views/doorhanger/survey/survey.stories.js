5554be8f427d97ea9c4bc48bcb5cdda5fa4503a2
collectedmind <joel@getpocket.com>
2019-08-28 00:22:29 -0700

    refactor(survey): use survey mock

--- src/components/views/doorhanger/survey/survey.stories.js
+++ src/components/views/doorhanger/survey/survey.stories.js
@@ -1,11 +1,7 @@
 import React from 'react'
-
+import survey from 'common/_mocks/surveyResponse.json'
 import { storiesOf } from '@storybook/react'
 import Survey from './survey.main'
 storiesOf('Views | Survey', module).add('main', () => (
-  <Survey
-    survey={{ show: true }}
-    surveyRespond={() => {}}
-    surveyCancel={() => {}}
-  />
+  <Survey survey={survey} surveyRespond={() => {}} surveyCancel={() => {}} />
 ))
