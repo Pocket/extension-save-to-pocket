08f6509b81838226235f51c032b980fccaf56544
collectedmind <joel@getpocket.com>
2019-08-27 22:51:28 -0700

    refactor(loader): moving loader to `elements`

--- src/components/elements/toggle/toggle.stories.js
+++ src/components/elements/toggle/toggle.stories.js
@@ -2,8 +2,7 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import Toggle from './toggle'
-storiesOf('Components | Toggle', module)
-  .add('active', () => <Toggle active={true}/>)
-  .add('inactive', () => <Toggle active={false}/>)
-  .add('darkmode', () => <Toggle darkMode={true}/>)
-
+storiesOf('Elements | Toggle', module)
+  .add('active', () => <Toggle active={true} />)
+  .add('inactive', () => <Toggle active={false} />)
+  .add('darkmode', () => <Toggle darkMode={true} />)
