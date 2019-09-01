e712a3d0c8b910e50bf4c14bd880cce4f08ca334
Nik Zeltzer <nzeltzer@gmail.com>
2019-09-01 15:07:21 -0700

    feat(safari): implement mac launcher design

--- _safari/Save to Pocket/ViewController.swift
+++ _safari/Save to Pocket/ViewController.swift
@@ -11,15 +11,36 @@ import SafariServices.SFSafariApplication
 
 class ViewController: NSViewController {
 
-    @IBOutlet var appNameLabel: NSTextField!
+    @IBOutlet var window: NSWindow!
+    @IBOutlet var buttonContainer: NSView!
+    @IBOutlet var buttonTitle: NSTextField!
+    @IBOutlet var gestureRecognizer: NSGestureRecognizer!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.view.wantsLayer = true
+        self.buttonContainer.wantsLayer = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.appNameLabel.stringValue = "Save to Pocket";
+        self.view.layer?.backgroundColor = CGColor.init(red: 1, green: 1, blue: 1, alpha: 1)
+        self.buttonContainer.layer?.backgroundColor = CGColor(red: 1, green: 0.2, blue: 0.33, alpha: 1)
+        self.buttonContainer.layer?.cornerRadius = 3
+    }
+    
+    @IBAction func didClickButton(click: NSClickGestureRecognizer) -> Void {
+        if click.state == .recognized {
+            self.buttonTitle.textColor = NSColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        }
+        if click.state == .ended {
+            self.buttonTitle.textColor = NSColor(red: 1, green: 1, blue: 1, alpha: 1)
+            self.openSafariExtensionPreferences(click)
+        }
     }
     
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.readitlater.Save-to-Pocket-Extension") { error in
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.pocket.safari.save") { error in
             if let _ = error {
                 // Insert code to inform the user that something went wrong.
 
