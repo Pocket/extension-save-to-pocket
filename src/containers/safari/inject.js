0e936ee4cc437c70c5dd7e2204982f028f142339
collectedmind <joel@getpocket.com>
2019-09-18 10:50:20 -0700

    fix(safari): check dom on save request

--- src/containers/safari/inject.js
+++ src/containers/safari/inject.js
@@ -9,35 +9,42 @@ import { MAIN_SCRIPT_INJECTED } from 'containers/safari/actions'
 import { Provider } from 'react-redux'
 import { store } from './store'
 //
+
+export function injectDomElements() {
+  const existingRoot = document.getElementById('pocket-extension-root')
+  if (existingRoot) return
+
+  const rootElement = document.createElement('div')
+  rootElement.id = 'pocket-extension-root'
+  const root = document.body.appendChild(rootElement)
+
+  const stylesElement = document.createElement('div')
+  stylesElement.id = 'pocket-extension-styles'
+  const styles = document.body.appendChild(stylesElement)
+
+  const myCache = createCache({
+    key: 'pocket',
+    container: styles
+  })
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <CacheProvider value={myCache}>
+        <SafariApp />
+      </CacheProvider>
+    </Provider>,
+    root,
+    () => {
+      safari.extension.dispatchMessage(MAIN_SCRIPT_INJECTED)
+    }
+  )
+}
+
 ;(function() {
   if (window.top === window) {
     function setLoaded() {
       dispatchInit()
-
-      const rootElement = document.createElement('div')
-      rootElement.id = 'pocket-extension-root'
-      const root = document.body.appendChild(rootElement)
-
-      const stylesElement = document.createElement('div')
-      stylesElement.id = 'pocket-extension-styles'
-      const styles = document.body.appendChild(stylesElement)
-
-      const myCache = createCache({
-        key: 'pocket',
-        container: styles
-      })
-
-      ReactDOM.render(
-        <Provider store={store}>
-          <CacheProvider value={myCache}>
-            <SafariApp />
-          </CacheProvider>
-        </Provider>,
-        root,
-        () => {
-          safari.extension.dispatchMessage(MAIN_SCRIPT_INJECTED)
-        }
-      )
+      injectDomElements()
     }
 
     // Check page has loaded and if not add listener for it
