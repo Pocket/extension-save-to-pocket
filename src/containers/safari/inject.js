c05b165738047ac850a14d060d3f020283ab7df3
collectedmind <joel@getpocket.com>
2019-10-05 15:04:46 -0700

    refactor(store): move store higher to be shared

--- src/containers/safari/inject.js
+++ src/containers/safari/inject.js
@@ -7,7 +7,7 @@ import { dispatchInit } from 'containers/safari/dispatch'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { store } from './store'
+import { store } from 'store'
 //
 
 export function injectDomElements() {
