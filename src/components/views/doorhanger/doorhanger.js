118fb8c89ba21d70fb9877e315e3dc4cc74f4e30
collectedmind <joel@getpocket.com>
2019-10-09 21:37:38 -0700

    refactor(doorhanger): updating animation library
    react-motion was not properly updated and was throwing warnings.  Pose is up to date and has a similar API

--- src/components/views/doorhanger/doorhanger.js
+++ src/components/views/doorhanger/doorhanger.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Toolbar from 'views/doorhanger/toolbar/toolbar.main'
 import Recommendations from 'views/doorhanger/recommendations/recommendations'
 import Survey from 'views/doorhanger/survey/survey.main'
-import { TransitionMotion, spring, presets } from 'react-motion' //
+import posed, { PoseGroup } from 'react-pose'
 import styled from '@emotion/styled'
 import { mixin_fontBase } from 'common/styles/components'
 import { withAutoHider } from 'modules/autohide/autohide.hoc'
@@ -16,6 +16,23 @@ const DoorHangerWrapper = styled.div`
   width: 320px;
   z-index: 2147483647;
 `
+
+const LoadInOut = posed.div({
+  enter: {
+    y: 0,
+    opacity: 1,
+    delay: 0,
+    transition: {
+      y: { type: 'spring', stiffness: 400, damping: 35 },
+      default: { duration: 500 }
+    }
+  },
+  exit: {
+    y: -240,
+    opacity: 0,
+    transition: { duration: 250 }
+  }
+})
 
 class DoorHanger extends Component {
   componentDidUpdate(prevProps) {
@@ -36,41 +53,6 @@ class DoorHanger extends Component {
     if (isSaveActive === prevProps.isSaveActive) return
 
     if (isSaveActive && beginTiming) beginTiming()
-  }
-
-  getDefaultStyles = () => {
-    return [
-      {
-        data: {},
-        key: 'toolbar',
-        style: {
-          transform: -240,
-          opacity: 0
-        }
-      }
-    ]
-  }
-
-  getStyles = () => {
-    return this.showing
-      ? [
-          {
-            data: {},
-            key: 'toolbar',
-            style: {
-              transform: spring(0, { stiffness: 350, damping: 25 }),
-              opacity: spring(1, presets.stiff)
-            }
-          }
-        ]
-      : []
-  }
-
-  willEnter() {
-    return { transform: -240, opacity: 0 }
-  }
-  willLeave() {
-    return { transform: spring(-240), opacity: spring(0) }
   }
 
   get showing() {
@@ -97,74 +79,58 @@ class DoorHanger extends Component {
 
   render() {
     return (
-      <TransitionMotion
-        defaultStyles={this.getDefaultStyles()}
-        styles={this.getStyles()}
-        willEnter={this.willEnter}
-        willLeave={this.willLeave}>
-        {items => {
-          return (
-            <div>
-              {items.map(item => {
-                return (
-                  <DoorHangerWrapper
-                    onMouseEnter={this.onHover}
-                    onMouseLeave={this.offHover}
-                    key={item.key}
-                    style={{
-                      transform: `translateY(${item.style.transform}%) translateZ(0)`,
-                      transformStyle: 'preserve-3d',
-                      opacity: item.style.opacity
-                    }}>
-                    <Toolbar
-                      tabId={this.props.tab_id}
-                      noSettings={this.props.noSettings}
-                      logOut={this.props.logOut}
-                      setDropDownStatus={this.props.setDropDownStatus}
-                      openPocket={this.props.openPocket}
-                      openOptions={this.props.openOptions}
-                      archive={this.props.archiveItem}
-                      remove={this.props.removeItem}
-                      currentTab={this.props.currentTab}
-                      active={this.props.active}
-                      tags={this.props.currentTags}
-                      activateTag={this.props.activateTag}
-                      deactivateTag={this.props.deactivateTag}
-                      deactivateTags={this.props.deactivateTags}
-                      addTag={this.props.addTag}
-                      closePanel={this.props.closePanel}
-                      removeTag={this.props.removeTag}
-                      removeTags={this.props.removeTags}
-                      storedTags={this.storedTags}
-                      inputFocused={this.props.inputFocused}
-                      setInputFocusState={this.props.setInputFocusState}
-                    />
-                    {this.props.showRecs && !this.showSurvey && (
-                      <Recommendations
-                        tabId={this.props.tab_id}
-                        recs={this.props.currentRecs}
-                        saveRecommendation={this.props.saveRecommendation}
-                        openRecommendation={this.props.openRecommendation}
-                        spocImpression={this.props.spocImpression}
-                        spocView={this.props.spocView}
-                        spocClick={this.props.spocClick}
-                        spocRemove={this.props.spocRemove}
-                      />
-                    )}
-                    {this.showSurvey && (
-                      <Survey
-                        survey={this.props.survey}
-                        surveyRespond={this.props.surveyRespond}
-                        surveyCancel={this.props.surveyCancel}
-                      />
-                    )}
-                  </DoorHangerWrapper>
-                )
-              })}
-            </div>
-          )
-        }}
-      </TransitionMotion>
+      <DoorHangerWrapper
+        onMouseEnter={this.onHover}
+        onMouseLeave={this.offHover}>
+        <PoseGroup>
+          {this.showing && [
+            <LoadInOut key="loadInOut">
+              <Toolbar
+                tabId={this.props.tab_id.toString()}
+                noSettings={this.props.noSettings}
+                logOut={this.props.logOut}
+                setDropDownStatus={this.props.setDropDownStatus}
+                openPocket={this.props.openPocket}
+                openOptions={this.props.openOptions}
+                archive={this.props.archiveItem}
+                remove={this.props.removeItem}
+                currentTab={this.props.currentTab}
+                active={this.props.active}
+                tags={this.props.currentTags}
+                activateTag={this.props.activateTag}
+                deactivateTag={this.props.deactivateTag}
+                deactivateTags={this.props.deactivateTags}
+                addTag={this.props.addTag}
+                closePanel={this.props.closePanel}
+                removeTag={this.props.removeTag}
+                removeTags={this.props.removeTags}
+                storedTags={this.storedTags}
+                inputFocused={this.props.inputFocused}
+                setInputFocusState={this.props.setInputFocusState}
+              />
+              {this.props.showRecs && !this.showSurvey && (
+                <Recommendations
+                  tabId={this.props.tab_id}
+                  recs={this.props.currentRecs}
+                  saveRecommendation={this.props.saveRecommendation}
+                  openRecommendation={this.props.openRecommendation}
+                  spocImpression={this.props.spocImpression}
+                  spocView={this.props.spocView}
+                  spocClick={this.props.spocClick}
+                  spocRemove={this.props.spocRemove}
+                />
+              )}
+              {this.showSurvey && (
+                <Survey
+                  survey={this.props.survey}
+                  surveyRespond={this.props.surveyRespond}
+                  surveyCancel={this.props.surveyCancel}
+                />
+              )}
+            </LoadInOut>
+          ]}
+        </PoseGroup>
+      </DoorHangerWrapper>
     )
   }
 }
