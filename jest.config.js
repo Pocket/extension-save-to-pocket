1c00c5dd9282b8edc83c4eb0a9c94096daf2792a
collectedmind <joel@getpocket.com>
2021-09-24 10:52:10 -0700

    build(config): add rollup/jest/mv3/etc

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupFilesAfterEnv: ['./config/jest.setup.js'],
+}
---
+++ rollup.config.js
@@ -0,0 +1,64 @@
+import path from 'path'
+import linaria from 'linaria/rollup'
+import css from 'rollup-plugin-css-only'
+import alias from '@rollup/plugin-alias'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import copy from 'rollup-plugin-copy'
+import resolve from '@rollup/plugin-node-resolve'
+import { chromeExtension } from 'rollup-plugin-chrome-extension'
+import { emptyDir } from 'rollup-plugin-empty-dir'
+import zip from 'rollup-plugin-zip'
+
+const isProduction = process.env.NODE_ENV === 'production'
+const projectRootDir = path.resolve(__dirname)
+
+export default {
+  input: 'src/manifest.json',
+  output: {
+    dir: 'build',
+    format: 'esm',
+    chunkFileNames: path.join('chunks', '[name]-[hash].js'),
+  },
+  plugins: [
+    chromeExtension(),
+    // Replace environment variables
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    resolve(),
+    commonjs(),
+    linaria(),
+    babel({
+      // Do not transpile dependencies
+      ignore: ['node_modules'],
+      babelHelpers: 'bundled',
+    }),
+    css({ output: 'assets/pocket-save-extension.css' }),
+
+    // Empties the output dir before a new build
+    emptyDir(),
+    copy({
+      targets: [
+        { src: 'src/assets/fonts/*', dest: 'build/assets/fonts' },
+        { src: 'src/assets/images/*', dest: 'build/assets/images' },
+      ],
+      hook: 'writeBundle',
+    }),
+    alias({
+      entries: {
+        actions: path.resolve(projectRootDir, 'src/actions.js'),
+        assets: path.resolve(projectRootDir, 'src/assets'),
+        common: path.resolve(projectRootDir, 'src/common'),
+        components: path.resolve(projectRootDir, 'src/components'),
+        connectors: path.resolve(projectRootDir, 'src/connectors'),
+        containers: path.resolve(projectRootDir, 'src/containers'),
+        pages: path.resolve(projectRootDir, 'src/pages'),
+      },
+    }),
+    // Outputs a zip file in ./releases
+    isProduction && zip({ dir: 'releases' }),
+  ],
+}
