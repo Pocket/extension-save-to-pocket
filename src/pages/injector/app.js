1d605f26c501b71c78e4a267233f193f2405fa81
collectedmind <joel@getpocket.com>
2021-09-25 18:31:56 -0700

    fix(content): remove listner on unmount

--- src/pages/injector/app.js
+++ src/pages/injector/app.js
@@ -12,55 +12,56 @@ import { REMOVE_ITEM_FAILURE } from 'actions'
 export const App = () => {
   const [saveStatus, setSaveStatus] = useState('idle')
 
+  /* Handle incoming messages
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+  const handleMessages = (event) => {
+    const { payload, action = 'Unknown Action' } = event || {}
+    console.groupCollapsed(`RECEIVE: ${action}`)
+    console.log(payload)
+    console.groupEnd(`RECEIVE: ${action}`)
+
+    switch (action) {
+      case SAVE_TO_POCKET_REQUEST: {
+        return setSaveStatus('saving')
+      }
+
+      case SAVE_TO_POCKET_SUCCESS: {
+        return setSaveStatus('saved')
+      }
+
+      case SAVE_TO_POCKET_FAILURE: {
+        return setSaveStatus('save_failed')
+      }
+
+      case ARCHIVE_ITEM_SUCCESS: {
+        return setSaveStatus('archived')
+      }
+
+      case ARCHIVE_ITEM_FAILURE: {
+        return setSaveStatus('archive_failed')
+      }
+
+      case REMOVE_ITEM_SUCCESS: {
+        return setSaveStatus('removed')
+      }
+
+      case REMOVE_ITEM_FAILURE: {
+        return setSaveStatus('remove_failed')
+      }
+
+      default: {
+        return
+      }
+    }
+  }
+
   useEffect(() => {
-    chrome.runtime.onMessage.addListener((event) =>
-      handleMessages(event, setSaveStatus),
-    )
     setSaveStatus('saving')
+    chrome.runtime.onMessage.addListener(handleMessages)
+    return () => {
+      chrome.runtime.onMessage.removeListener(handleMessages)
+    }
   }, [])
 
   return <Doorhanger saveStatus={saveStatus} />
-}
-
-/* Handle incoming messages
-–––––––––––––––––––––––––––––––––––––––––––––––––– */
-const handleMessages = (event, setSaveStatus) => {
-  const { payload, action = 'Unknown Action' } = event || {}
-  console.groupCollapsed(`RECEIVE: ${action}`)
-  console.log(payload)
-  console.groupEnd(`RECEIVE: ${action}`)
-
-  switch (action) {
-    case SAVE_TO_POCKET_REQUEST: {
-      return setSaveStatus('saving')
-    }
-
-    case SAVE_TO_POCKET_SUCCESS: {
-      return setSaveStatus('saved')
-    }
-
-    case SAVE_TO_POCKET_FAILURE: {
-      return setSaveStatus('save_failed')
-    }
-
-    case ARCHIVE_ITEM_SUCCESS: {
-      return setSaveStatus('archived')
-    }
-
-    case ARCHIVE_ITEM_FAILURE: {
-      return setSaveStatus('archive_failed')
-    }
-
-    case REMOVE_ITEM_SUCCESS: {
-      return setSaveStatus('removed')
-    }
-
-    case REMOVE_ITEM_FAILURE: {
-      return setSaveStatus('remove_failed')
-    }
-
-    default: {
-      return
-    }
-  }
 }
