77dafa10b047b7c2ca2c70799a403fbe8f8e8e35
Anthony Liddle <anthonyliddle@gmail.com>
2021-09-27 14:23:53 -0700

    fix(cleanup): removing old rec and survey files

--- src/containers/doorhanger/recommendations/header/header.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import { storiesOf } from '@storybook/react'
-import Header from './header'
-storiesOf('Views | Recommendations/Header', module)
-  .add('reason', () => <Header recs={{ reason: 'Chemistry', feed: [1, 2] }} />)
-  .add('no reason', () => <Header recs={{ reason: '', feed: [1, 2] }} />)
-  .add('no results', () => <Header recs={{ reason: '' }} />)
-  .add('loading', () => <Header />)
