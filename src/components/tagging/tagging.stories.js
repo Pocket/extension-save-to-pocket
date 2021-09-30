1993a3049e74123bc9a368c79d15eeefec4dfc69
Aly Fluckey <afluckey@mozilla.com>
2021-09-30 12:32:38 -0500

    fix(tagging): add tagging story

---
+++ src/components/tagging/tagging.stories.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Tagging as TaggingComponent } from './tagging'
+
+export default {
+  title: 'Components',
+};
+
+const usedTags = ['test', 'space']
+const markedTags = ['space']
+const suggestedTags = ['pink elephants', 'lava lamps']
+const storedTags = ['jamba juice', 'donut time']
+const addTag = () => {}
+const activateTag = () => {}
+const deactivateTag = () => {}
+const deactivateTags = () => {}
+const removeTag = () => {}
+const removeTags = () => {}
+const closePanel = () => {}
+
+export const Tagging = () => {
+  return (
+    <TaggingComponent
+      usedTags={usedTags}
+      markedTags={markedTags}
+      suggestedTags={suggestedTags}
+      storedTags={storedTags}
+      addTag={addTag}
+      activateTag={activateTag}
+      deactivateTag={deactivateTag}
+      deactivateTags={deactivateTags}
+      removeTag={removeTag}
+      removeTags={removeTags}
+      closePanel={closePanel}
+    />
+  )
+}
