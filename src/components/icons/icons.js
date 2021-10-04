8e707af18964eea050523b47490c2090cc9cf78b
Aly Fluckey <afluckey@mozilla.com>
2021-10-04 16:54:06 -0500

    fix(icons): add twitter icon

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Icon from './icon'
 import PocketLogo from './svg/PocketLogo'
 import Settings from './svg/Settings'
+import TwitterMono from './svg/Twitter-Mono'
 
 export const PocketLogoIcon = (props) => (
   <Icon {...props}>
@@ -13,5 +14,11 @@ export const PocketLogoIcon = (props) => (
 export const SettingsIcon = (props) => (
   <Icon {...props}>
     <Settings />
+  </Icon>
+)
+
+export const TwitterIcon = (props) => (
+  <Icon {...props}>
+    <TwitterMono />
   </Icon>
 )
