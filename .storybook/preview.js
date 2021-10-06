09ab9008bb19905c0f12a8a86e4b6dadd3cfe9bf
Anthony Liddle <anthonyliddle@gmail.com>
2021-10-06 12:04:34 -0700

    fix(storybook): updating the preview to allow for theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,21 +1,46 @@
 import React from 'react'
 import { GlobalVariables } from "../src/pages/injector/globalStyles"
-import { cx } from 'linaria'
+import { css, cx } from 'linaria'
+
+const storyWrapperStyles = css`
+  height: 100vh;
+  width: 100vw;
+  padding: 10px;
+  box-sizing: border-box;
+
+  &.pocket-theme-dark {
+    background-color: black;
+  }
+`
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
+  layout: 'fullscreen'
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'chromatic',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark']
+    }
+  }
 }
 
 export const decorators = [
-  (Story) => (
-    <div className={cx(GlobalVariables, 'pocket-theme-light')}>
+  (Story, context) => (
+    <div className={cx(GlobalVariables, storyWrapperStyles, `pocket-theme-${context.globals.theme}`)}>
       <Story />
     </div>
-  ),
+  )
 ]
