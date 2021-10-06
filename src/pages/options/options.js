2ffa97f5eedea061e1805bb85b5e3e1b3221c43e
Aly Fluckey <afluckey@mozilla.com>
2021-10-06 15:04:44 -0500

    fix(options): styling

--- src/pages/options/options.js
+++ src/pages/options/options.js
@@ -27,21 +27,21 @@ const optionsContainer = css`
   ${GlobalVariables};
   box-sizing: border-box;
   display: inline-block;
-  margin-bottom: 80px;
   padding: 0 30px;
-  width: 700px;
-
-  @media (min-width: 320px) and (max-width: 767px) {
-    width: 100%;
-  }
+  width: 100%;
+`
+const optionsWrapper = css`
+  max-width: 700px;
+  margin: 0 auto;
 `
 const optionsTitle = css`
-  color: ${$pitch};
-  font-size: 28px;
-  font-weight: 300;
-  margin: 70px 0 0;
-  padding: 0 0 20px;
-  text-align: left;
+  color: var(--color-textPrimary);
+  font-size: 33px;
+  line-height: 40px;
+  font-weight: 600;
+`
+const optionsHeader = css`
+  border-bottom: 1px solid var(--color-dividerPrimary);
 `
 const optionsSection = css`
   align-items: center;
@@ -118,153 +118,154 @@ const OptionsApp = () => {
 
   return (
     <div className={cx(optionsContainer, `pocket-theme-${pageTheme}`)}>
-      <Logo />
-      <h1 className={cx(optionsTitle)}>
-        {localize('options', 'header')}
-      </h1>
+      <section className={optionsWrapper}>
+        <div className={optionsHeader}>
+          <Logo />
+          <h1 className={cx(optionsTitle)}>
+            {localize('options', 'header')}
+          </h1>
+        </div>
 
-      <div className={optionsSection}>
-        <div className={optionsSectionContent}>
-          <div className={optionsSectionTitle}>
-            {localize('options', 'login_title')}
-          </div>
-          <div className={optionsSectionMain}>
-            {(accessToken && userName) ? (
-              <div>
-                {userName}
-                <Button type='secondary' onClick={logoutAction}>
-                  {localize('options', 'log_out')}
+        <div className={optionsSection}>
+          <div className={optionsSectionContent}>
+            <div className={optionsSectionTitle}>
+              {localize('options', 'login_title')}
+            </div>
+            <div className={optionsSectionMain}>
+              {(accessToken && userName) ? (
+                <div>
+                  {userName}
+                  <Button type='secondary' onClick={logoutAction}>
+                    {localize('options', 'log_out')}
+                  </Button>
+                </div>
+              ) : (
+                <Button type='secondary' onClick={loginAction}>
+                  {localize('options', 'log_in')}
                 </Button>
-              </div>
-            ) : (
-              <Button type='secondary' onClick={loginAction}>
-                {localize('options', 'log_in')}
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className={optionsSection}>
+          <div className={optionsSectionContent}>
+            <div className={optionsSectionTitle}>
+              {localize('options', 'shortcut_title')}
+            </div>
+            <div className={optionsSectionMain}>
+              <Button type='primary' onClick={setShortcuts}>
+                {localize('options', 'shortcut_record')}
               </Button>
-            )}
-          </div>
-        </div>
-      </div>
-
-      <div className={optionsSection}>
-        <div className={optionsSectionContent}>
-          <div className={optionsSectionTitle}>
-            {localize('options', 'shortcut_title')}
-          </div>
-          <div className={optionsSectionMain}>
-            <Button type='primary' onClick={setShortcuts}>
-              {localize('options', 'shortcut_record')}
-            </Button>
-          </div>
-        </div>
-      </div>
-
-      <div className={optionsSection}>
-        <div className={optionsSectionContent}>
-          <div className={cx(optionsSectionTitle)}>
-            {localize('options', 'theme_title')}
-          </div>
-          <div className={cx(optionsSectionMain)}>
-            <div>
-              <input
-                id="light"
-                type="radio"
-                name="light"
-                onChange={() => updateTheme('light')}
-                checked={storedTheme === 'light'}
-              />
-              <label htmlFor="light">{localize('options', 'theme_light')}</label>
-            </div>
-            <div>
-              <input
-                id="dark"
-                type="radio"
-                name="dark"
-                onChange={() => updateTheme('dark')}
-                checked={storedTheme === 'dark'}
-              />
-              <label htmlFor="dark">{localize('options', 'theme_dark')}</label>
-            </div>
-            <div>
-              <input
-                id="system"
-                type="radio"
-                name="system"
-                onChange={() => updateTheme('system')}
-                checked={storedTheme === 'system'}
-              />
-              <label htmlFor="system">{localize('options', 'theme_system')}</label>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className={optionsSection}>
-        <div className={optionsSectionContent}>
-          <div className={optionsSectionTitle}>
-            {localize('options', 'app_title')}
-          </div>
-          <div className={optionsSectionMain}>
-            <a
-              href="https://apps.apple.com/us/app/pocket-save-read-grow/id309601447"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img
-                src="https://assets.getpocket.com/web-ui/assets/apple-app-store-badge.2928664fe1fc6aca88583a6f606d60ba.svg"
-                alt={localize('options', 'app_apple')}
-              />
-            </a>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.ideashower.readitlater.pro"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img
-                src="https://assets.getpocket.com/web-ui/assets/google-play-badge.db9b21a1c41f3dcd9731e1e7acfdbb57.png"
-                alt={localize('options', 'app_google')}
-              />
-            </a>
+        <div className={optionsSection}>
+          <div className={optionsSectionContent}>
+            <div className={cx(optionsSectionTitle)}>
+              {localize('options', 'theme_title')}
+            </div>
+            <div className={cx(optionsSectionMain)}>
+              <div>
+                <input
+                  id="light"
+                  type="radio"
+                  name="light"
+                  onChange={() => updateTheme('light')}
+                  checked={storedTheme === 'light'}
+                />
+                <label htmlFor="light">{localize('options', 'theme_light')}</label>
+              </div>
+              <div>
+                <input
+                  id="dark"
+                  type="radio"
+                  name="dark"
+                  onChange={() => updateTheme('dark')}
+                  checked={storedTheme === 'dark'}
+                />
+                <label htmlFor="dark">{localize('options', 'theme_dark')}</label>
+              </div>
+              <div>
+                <input
+                  id="system"
+                  type="radio"
+                  name="system"
+                  onChange={() => updateTheme('system')}
+                  checked={storedTheme === 'system'}
+                />
+                <label htmlFor="system">{localize('options', 'theme_system')}</label>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <footer className={optionsFooter}>
-        <a
-          href="https://help.getpocket.com/"
-          target="_blank"
-          rel="noopener noreferrer">
-          {localize('options', 'need_help')}
-        </a>
-
-        <a
-          href="https://getpocket.com/contact_support?field3=Question%20about%20Pocket%20Extension"
-          target="_blank"
-          rel="noopener noreferrer">
-          {localize('options', 'email_us')}
-        </a>
-
-        <div>
-          {localize('options', 'follow')}
-          <FacebookIcon />
-          <TwitterIcon />
+        <div className={optionsSection}>
+          <div className={optionsSectionContent}>
+            <div className={optionsSectionTitle}>
+              {localize('options', 'app_title')}
+            </div>
+            <div className={optionsSectionMain}>
+              <a
+                href="https://apps.apple.com/us/app/pocket-save-read-grow/id309601447"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img
+                  src="https://assets.getpocket.com/web-ui/assets/apple-app-store-badge.2928664fe1fc6aca88583a6f606d60ba.svg"
+                  alt={localize('options', 'app_apple')}
+                />
+              </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.ideashower.readitlater.pro"
+                target="_blank"
+                rel="noopener noreferrer">
+                <img
+                  src="https://assets.getpocket.com/web-ui/assets/google-play-badge.db9b21a1c41f3dcd9731e1e7acfdbb57.png"
+                  alt={localize('options', 'app_google')}
+                />
+              </a>
+            </div>
+          </div>
         </div>
+        <footer className={optionsFooter}>
+          <a
+            href="https://help.getpocket.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            {localize('options', 'need_help')}
+          </a>
 
-        <PocketLogoIcon />
-        <p dangerouslySetInnerHTML={{ __html: localize('options', 'family').replace('Mozilla', '<a href="https://mozilla.org/about/" rel="noopener noreferrer" target="_blank">Mozilla</a>')}}></p>
+          <a
+            href="https://getpocket.com/contact_support?field3=Question%20about%20Pocket%20Extension"
+            target="_blank"
+            rel="noopener noreferrer">
+            {localize('options', 'email_us')}
+          </a>
 
-        <p>&copy; {new Date().getFullYear()} Read It Later, Inc.</p>
+          <div>
+            {localize('options', 'follow')}
+            <FacebookIcon />
+            <TwitterIcon />
+          </div>
 
-        <a
-          href='https://getpocket.com/privacy/?src=extension'
-          rel='noopener noreferrer'
-          target='_blank'>
-          {localize('options', 'privacy')}
-        </a>
-        <a
-          href='https://getpocket.com/tos/?src=extension'
-          rel='noopener noreferrer'
-          target='_blank'>
-          {localize('options', 'terms')}
-        </a>
-      </footer>
+          <PocketLogoIcon />
+          <p dangerouslySetInnerHTML={{ __html: localize('options', 'family').replace('Mozilla', '<a href="https://mozilla.org/about/" rel="noopener noreferrer" target="_blank">Mozilla</a>')}}></p>
+
+          <p>&copy; {new Date().getFullYear()} Read It Later, Inc.</p>
+
+          <a
+            href='https://getpocket.com/privacy/?src=extension'
+            rel='noopener noreferrer'
+            target='_blank'>
+            {localize('options', 'privacy')}
+          </a>
+          <a
+            href='https://getpocket.com/tos/?src=extension'
+            rel='noopener noreferrer'
+            target='_blank'>
+            {localize('options', 'terms')}
+          </a>
+        </footer>
+      </section>
     </div>
   )
 }
