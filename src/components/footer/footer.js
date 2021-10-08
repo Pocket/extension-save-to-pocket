2ee451987b471c708f1dc6b9e80dde4e4a8a9bf9
Aly Fluckey <afluckey@mozilla.com>
2021-10-08 12:54:45 -0500

    fix(footer): fix background color and settings icon color

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,6 +5,7 @@ import { SettingsIcon } from 'components/icons/icons'
 import { Button } from 'components/button/button'
 
 const footerWrapper = css`
+  background-color: var(--color-canvas);
   display: flex;
   justify-content: space-between;
   border-top: 1px solid var(--color-dividerTertiary);
@@ -16,13 +17,21 @@ const footerWrapper = css`
     width: 25px;
     margin-right: 8px;
   }
+
+  .settings {
+    color: var(--color-textPrimary);
+    
+    &:hover {
+      color: var(--color-actionPrimary);
+    }
+  }
 `
 
 export const Footer = ({ myListClick, settingsClick }) => {
   return (
     <footer className={footerWrapper}>
       <Button type="secondary" onClick={myListClick}>{localize('buttons', 'mylist')}</Button>
-      <Button type="inline" onClick={settingsClick}>
+      <Button className="settings" type="inline" onClick={settingsClick}>
         <SettingsIcon />
       </Button>
     </footer>
