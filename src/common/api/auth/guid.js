a9793ae67f0c6c2d54d397132630e50564048a54
collectedmind <joel@getpocket.com>
2021-12-24 12:36:03 -0800

    refactor(guid): simplify async calls

--- src/common/api/auth/guid.js
+++ src/common/api/auth/guid.js
@@ -2,40 +2,32 @@ import { request } from '../_request/request'
 import { getSetting } from '../../interface'
 import { arrayHasValues } from '../../utilities'
 
-export function getGuid() {
-  return new Promise((resolve, reject) => {
-    return Promise.all([getExtensionGuid(), getSiteGuid()])
-      .then((resolvedValues) => {
-        if (arrayHasValues(resolvedValues).length) {
-          // Extension Guid
-          if (resolvedValues[0])
-            resolve({
-              source: 'extension',
-              guid: resolvedValues[0],
-            })
-          // Site Guid
-          if (resolvedValues[1])
-            resolve({
-              source: 'site',
-              guid: resolvedValues[1].value,
-            })
-        } else {
-          // Server Guid
-          getServerGuid()
-            .then((data) => resolve({ source: 'server', guid: data.guid }))
-            .catch((err) => reject(err))
-        }
-      })
-      .catch((err) => reject(err))
-  })
+export async function getGuid() {
+  const extensionGuid = await getExtensionGuid()
+  if (extensionGuid) return extensionGuid
+
+  const siteGuid = await getSiteGuid()
+  if (siteGuid) return siteGuid
+
+  const serverGuid = await getServerGuid()
+  if (serverGuid) return serverGuid
+
+  return false
 }
 
-export function getServerGuid() {
-  return request({ path: 'guid', data: { abt: 1 } })
+export async function getServerGuid() {
+  try {
+    return await request({ path: 'guid', data: { abt: 1 } }).then(
+      (data) => data.guid,
+    )
+  } catch (err) {
+    console.info(err)
+  }
 }
 
-export function getExtensionGuid() {
-  return new Promise((resolve) => resolve(getSetting('guid')))
+export async function getExtensionGuid() {
+  const guid = await getSetting('guid')
+  return guid ? guid : false
 }
 
 export async function getSiteGuid() {
@@ -44,5 +36,5 @@ export async function getSiteGuid() {
     name: 'sess_guid',
   })
 
-  return cookies
+  return cookies?.value
 }
